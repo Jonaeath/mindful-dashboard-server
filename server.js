f76bd9c9eb3a8fc42779a300d4c129f8094819cd
Jonaira Manha <abcd@Jonaeath.local>
2023-12-30 20:02:59 +0600

    add user

--- server.js
+++ server.js
@@ -10,6 +10,40 @@ app.use(express.json());
 
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.MD_USER}:${process.env.MD_PASSWORD}@cluster0.pg0dj0q.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+ 
+    const userDatabase = client.db("mindfulDashboard").collection("users")
+
+    
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const userData = userDatabase.insertOne(user);
+      res.send(userData);
+    });
+
+    
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
+
 
 app.get('/',(req,res)=>{
     res.send('Start up server is running')
